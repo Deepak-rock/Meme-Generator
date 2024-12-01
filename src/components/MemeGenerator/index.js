d99fb9526fc9a1c615180d6bc8dc44b9967e61df
Deepak-rock <deepakind777@gmail.com>
2024-12-01 15:56:07 +0530

    test 1

--- src/components/MemeGenerator/index.js
+++ src/components/MemeGenerator/index.js
@@ -1,3 +1,19 @@
+import {Component} from 'react'
+import {
+  AppContainer,
+  ResponsiveContainer,
+  MemeGeneratorContainer,
+  Heading,
+  FormContainer,
+  CustomLabel,
+  CustomInput,
+  CustomSelect,
+  CustomOption,
+  CustomButton,
+  MemeContainer,
+  TextContent,
+} from './styledComponents'
+
 const fontSizesOptionsList = [
   {
     optionId: '8',
@@ -29,3 +45,123 @@ const fontSizesOptionsList = [
   },
 ]
 // Write your code here
+class MemeGenerator extends Component {
+  state = {
+    imgUrlInput: '',
+    topTextInput: '',
+    bottomTextInput: '',
+    fontSizeInput: fontSizesOptionsList[0].optionId,
+    backgroundImgUrl: '',
+    topText: '',
+    bottomText: '',
+    fontSize: '',
+  }
+
+  onChangeImageUrl = event => {
+    this.setState({imgUrlInput: event.target.value})
+  }
+
+  onChangeToptext = event => {
+    this.setState({topTextInput: event.target.value})
+  }
+
+  onChangeBottomtext = event => {
+    this.setState({bottomTextInput: event.target.value})
+  }
+
+  onChangeFontsize = event => {
+    this.setState({fontSizeInput: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {
+      imgUrlInput,
+      topTextInput,
+      bottomTextInput,
+      fontSizeInput,
+    } = this.state
+
+    this.setState({
+      backgroundImgUrl: imgUrlInput,
+      topText: topTextInput,
+      bottomText: bottomTextInput,
+      fontSize: fontSizeInput,
+    })
+  }
+
+  renderForm = () => {
+    const {
+      imgUrlInput,
+      topTextInput,
+      bottomTextInput,
+      fontSizeInput,
+    } = this.state
+    return (
+      <FormContainer onSubmit={this.onSubmitForm}>
+        <CustomLabel htmlFor="imageUrl">Image URL</CustomLabel>
+        <CustomInput
+          id="imageUrl"
+          type="text"
+          placeholder="Enter the Image URL"
+          value={imgUrlInput}
+          onChange={this.onChangeImageUrl}
+        />
+        <CustomLabel htmlFor="topText">Top Text</CustomLabel>
+        <CustomInput
+          id="topText"
+          type="text"
+          placeholder="Enter the Top Text"
+          value={topTextInput}
+          onChange={this.onChangeToptext}
+        />
+        <CustomLabel htmlFor="bottomText">Bottom Text</CustomLabel>
+        <CustomInput
+          id="bottomText"
+          type="text"
+          placeholder="Enter the Bottom Text"
+          value={bottomTextInput}
+          onChange={this.onChangeBottomtext}
+        />
+        <CustomLabel htmlFor="select">Font Size</CustomLabel>
+        <CustomSelect
+          id="select"
+          onChange={this.onChangeFontsize}
+          value={fontSizeInput}
+        >
+          {fontSizesOptionsList.map(eachOption => (
+            <CustomOption key={eachOption.optionId} value={eachOption.optionId}>
+              {eachOption.displayText}
+            </CustomOption>
+          ))}
+        </CustomSelect>
+        <CustomButton type="submit">Generate</CustomButton>
+      </FormContainer>
+    )
+  }
+
+  renderMeme = () => {
+    const {backgroundImgUrl, topText, bottomText, fontSize} = this.state
+    return (
+      <MemeContainer data-testid="meme" backgroundImage={backgroundImgUrl}>
+        <TextContent fontSize={fontSize}>{topText}</TextContent>
+        <TextContent fontSize={fontSize}>{bottomText}</TextContent>
+      </MemeContainer>
+    )
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <ResponsiveContainer>
+          <Heading>Meme Generator</Heading>
+          <MemeGeneratorContainer>
+            {this.renderMeme()}
+            {this.renderForm()}
+          </MemeGeneratorContainer>
+        </ResponsiveContainer>
+      </AppContainer>
+    )
+  }
+}
+export default MemeGenerator
--- src/components/MemeGenerator/styledComponents.js
+++ src/components/MemeGenerator/styledComponents.js
@@ -1,1 +1,108 @@
 // Style your components here
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  min-height: 100vh;
+  padding: 50px 30px;
+`
+export const ResponsiveContainer = styled.div`
+  width: 100%;
+  max-width: 1100px;
+`
+export const MemeGeneratorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    gap: 30px;
+  }
+`
+export const Heading = styled.h1`
+  color: #35469c;
+  font-family: 'roboto';
+  font-weight: bold;
+  font-size: 32px;
+  text-align: center;
+  @media screen and (min-width: 768px) {
+    text-align: left;
+    font-size: 48px;
+  }
+`
+export const FormContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-item: flex-start;
+  @media screen and (min-width: 768px) {
+    width: 50%;
+  }
+`
+export const CustomLabel = styled.label`
+  color: #7e858e;
+  font-size: 16px;
+  font-family: 'roboto';
+  line-height: 1.5;
+`
+export const CustomInput = styled.input`
+  margin: 0 0 15px 0;
+  font-size: 16px;
+  font-family: 'roboto';
+  color: #5a7184;
+  padding: 8px 10px;
+  border-radius: 5px;
+  outline: none;
+  border: 1px solid #7e858e;
+  cursor: pointer;
+`
+export const CustomSelect = styled.select`
+  margin: 0 0 15px 0;
+  padding: 8px 10px;
+  font-size: 16px;
+  font-family: 'roboto';
+  color: #5a7184;
+  border-radius: 5px;
+  outline: none;
+  border: 1px solid #7e858e;
+  cursor: pointer;
+`
+export const CustomOption = styled.option`
+  font-size: 16px;
+  font-family: 'roboto';
+  color: #5a7184;
+  cursor: pointer;
+`
+export const CustomButton = styled.button`
+  background-color: #0b69ff;
+  color: #ffffff;
+  font-size: 16px;
+  font-family: 'roboto';
+  padding: 8px 35px;
+  border: none;
+  outline: none;
+  align-self: flex-start;
+  border-radius: 5px;
+  cursor: pointer;
+`
+export const MemeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-image: url(${props => props.backgroundImage});
+  background-size: cover;
+  background-position: center;
+  padding: 20px;
+  margin: 20px 0;
+  @media screen and (min-width: 768px) {
+    order: 2;
+    width: 50%;
+  }
+`
+export const TextContent = styled.p`
+  color: #ffffff;
+  font-size: ${props => props.fontSize}px;
+  font-family: 'roboto';
+`
